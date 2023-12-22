9bb18d9bb619c0c4ce3ebb75a131bdbf2906ef14
tutulhaque <sdtutulctg@gmail.com>
2023-12-22 18:27:36 +0600

    task management server created

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z9xqo9p.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
